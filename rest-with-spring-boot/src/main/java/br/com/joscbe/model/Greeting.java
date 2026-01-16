18128eb8ffb0f8022192adaa30ae6f2816a4c4d6
Josebe Barbosa <josebe.job@gmail.com>
2026-01-15 21:02:21 -0400

    First Steps in Java With Spring Boot 3.4.0

---
+++ rest-with-spring-boot/src/main/java/br/com/joscbe/model/Greeting.java
@@ -0,0 +1,3 @@
+package br.com.joscbe.model;
+
+public record Greeting(long id, String content) {}
---
+++ rest-with-spring-boot/src/test/java/br/com/joscbe/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.joscbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
