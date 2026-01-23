5a3bdb7ad82d5004d268a01d336880ece981cc9a
Josebe Barbosa <josebe.job@gmail.com>
2026-01-22 20:24:00 -0400

    Path Params section 5

---
+++ rest-with-spring-boot/src/main/java/br/com/joscbe/utils/NumberUtils.java
@@ -0,0 +1,18 @@
+package br.com.joscbe.utils;
+
+import br.com.joscbe.exception.UnsupportedMathOperationException;
+
+public class NumberUtils {
+
+    public static Double convertToDouble(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("please set a numeric value");
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric(String strNumber){
+        if(strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
